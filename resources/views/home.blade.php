420c866bc0375e7e631858cbee84174f167631af
Naveen Reddy <Naveen Reddy>
2017-03-11 19:45:07 -0600

    adding omk to git repo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-<h2 align="center"> <a href="http://localhost/omc2/public/mentors" style="font-size:75"> Mentor  </a> | <a href="http://localhost/omc2/public/students" style="font-size:75;">Student </a> | <a href="http://omk.com/Staff" style="font-size:75;">  Staff </a> | <a href="http://omk.com/Admin" style="font-size:75;"> Admin </a> </h2>        
+<h2 align="center"> <a href="http://omk.herokuapp.com/home/mentors" style="font-size:75"> Mentor  </a> | <a href="http://omk.herokuapp.com/home/students" style="font-size:75;">Student </a> | <a href="http://omk.herokuapp.com/home/staffs" style="font-size:75;">  Staff </a> | <a href="http://omk.herokuapp.com/home/admins" style="font-size:75;"> Admin </a> </h2>        
 
                 <div class="panel-body">
                     You are logged in!
