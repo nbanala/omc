2a8c58cef47f9cf4f6223c38b40272da625502bc
Naveen Reddy <Naveen Reddy>
2017-04-05 11:17:58 -0500

    home.blade .php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -24,9 +24,7 @@
 
 <h2 align="center"><a href={{action('AdminController@index')}} style="font-size:75;"> Admin </a> </h2>
 @endif
-<!--<h2 align="center"> <a href="http://omk.herokuapp.com/mentors" style="font-size:75"> Mentor  </a> | <a href="http://omk.herokuapp.com/students" style="font-size:75;">Student </a> | <a href="http://localhost/omc2/public/staffs" style="font-size:75;">  Staff </a> | <a href="http://omk.herokuapp.com/admins" style="font-size:75;"> Admin </a> </h2>-->
-
-                <div class="panel-body"><h2 align="center">
+               <div class="panel-body"><h2 align="center">
                     Click to  continue!
                 </div>
 @endif
